a8c54b64b2055f3e4c75381c0a8f5dc19f4c8fc3
jiteshbharati90 <jiteshbharati90@gmail.com>
2023-10-17 21:51:34 +0530

    jitesh

---
+++ Array/Print_Array.java
@@ -0,0 +1,14 @@
+public class Print_Array {
+    public static void main(String[] args) {
+        int arr[] = {10, 12, 23, 5, 65, 76, 7, 6, 54, 33};
+
+
+        for (int jitesh: arr
+             ) {
+
+            System.out.print(jitesh+"  ");
+
+
+        }
+    }
+}
